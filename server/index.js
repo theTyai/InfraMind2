0b72c5f2bc8848c340622556547c51df5fb025d6
theTyai <work.mishraashish@gmail.com>
2026-05-27 03:13:42 +0545

    implemented server hardening and initialize express entry point while removing outdated deployment guide

--- server/index.js
+++ server/index.js
@@ -35,6 +35,7 @@ app.use(helmet({
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://infra-mind2.vercel.app/',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
